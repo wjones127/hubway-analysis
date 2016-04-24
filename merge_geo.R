703a7c52ddcd93f5590dd9786c1736ed0307764c
Will Jones <thefreethinker127@gmail.com>
2016-04-24 13:09:10 -0700

    Finish census data merge

--- merge_geo.R
+++ merge_geo.R
@@ -13,18 +13,36 @@ census <- read.csv("data/census.csv")
 qmap(location = "boston") + 
   geom_point(aes(x = lng, y = lat, color = status), data = stations)
 
-ogrInfo(dsn = "data/census2010_tracts", layer = "Census2010_Tracts")
-tract_map <- readOGR(dsn = "data/census2010_tracts", layer = "Census2010_Tracts") %>%
+  ogrInfo(dsn = "data/census_2010_tracts", layer = "CENSUS2010TRACTS_POLY")
+tract_map <- readOGR(dsn = "data/census_2010_tracts", layer = "CENSUS2010TRACTS_POLY") %>%
   spTransform(CRS("+init=epsg:4326")) # converting coordinates
 tracts_data <- tract_map@data %>% rename(fips = GEOID10)
 tracts <- tidy(tract_map) %>% filter(hole == FALSE)
 
-qmap(location = "boston", zoom = 12) + 
+# Plot all census tracts
+qmap(location = "boston") + 
   geom_polygon(aes(x = long, y = lat, group = group), data = tracts,
+               fill = "steelblue", color = "black", alpha = 0.4)
+
+
+# need to narrow down census tracts for speed
+long_lims <- range(stations$lng)
+lat_lims <- range(stations$lat)
+
+tracts_in_range <- filter(tracts, between(long, long_lims[1], long_lims[2]) &
+                          between(lat, lat_lims[1], lat_lims[2]))$id %>%
+  unique()
+
+tracts_local <- filter(tracts, id %in% tracts_in_range)
+
+
+qmap(location = "boston", zoom = 12) + 
+  geom_polygon(aes(x = long, y = lat, group = group), data = tracts_local,
                fill = "steelblue", color = "black", alpha = 0.4) + 
   geom_point(aes(x = lng, y = lat, color = status), data = stations) + 
   coord_map(xlim = range(stations$lng) + c(-.02, .02) , 
             ylim = range(stations$lat) + c(-.02, .02))
+
 
 #' @function assign_census_tract: give lat and long, assigns a census tract to
 #' that point.
@@ -32,11 +50,11 @@ assign_census_tract <- function(coords) {
   lat <- coords[1]
   long <- coords[2]
   
-  num_tracts <- tracts$id %>% unique() %>% length()
-  for (i in 0:(num_tracts-1)) {
+  num_tracts <- tracts_local$id %>% unique() %>% length()
+  for (i in tracts_in_range) {
     # pull together data
-    tract_longs <- filter(tracts, id == i)$long
-    tract_lats <- filter(tracts, id == i)$lat
+    tract_longs <- filter(tracts_local, id == i)$long
+    tract_lats <- filter(tracts_local, id == i)$lat
     
     if (point.in.polygon(long, lat, tract_longs, tract_lats, mode.checked = TRUE)) {
       return(tracts_data[i,]$fips)
@@ -56,3 +74,6 @@ stations$fips <- get_tracts(stations$lat, stations$lng)
 
 
 stations <- left_join(stations, census, by = c("fips" = "FIPS"))
+
+
+save(stations, file = "stations.RData")
